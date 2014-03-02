3d7c8298225eb606adc08ab6742529d47e3dacbd
David Aragon <david@quickleft.com>
2014-03-02 16:21:38 -0700

    user can delete posts

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -7,11 +7,24 @@ App.PostController = Ember.ObjectController.extend({
 			this.get('model').save().then(function() {
 				this.set('isEditing', false);
 			}.bind(this));
+		},
+		delete: function() {
+			if (confirm("Delete this post?")) {
+				console.log(this.get('model'))
+				this.get('model').deleteRecord();
+				this.get('model').save().then(function() {
+					this.transitionToRoute('posts');
+				}.bind(this));
+			}
 		}
 	},
 	setIsOwner: function() {
 		var _this = this;
 		return this.get('model').get('author').then(function(author) {
+			if (!author) {
+				_this.set('isOwner', false);
+				return false;
+			}
 			_this.set('isOwner', (author.get('id') === _this.get('currentUser').id.toString()))
 			return (author.get('id') === _this.get('currentUser').id.toString())
 		});
