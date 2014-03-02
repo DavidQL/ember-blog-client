32ed5793a043bea339d2f0a86fdf8f450a7d313b
David Aragon <david@quickleft.com>
2014-03-02 16:06:34 -0700

    user can edit his posts

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -1,4 +1,14 @@
 App.PostController = Ember.ObjectController.extend({
+	actions: {
+		edit: function() {
+			this.set('isEditing', true);
+		},
+		save: function() {
+			this.get('model').save().then(function() {
+				this.set('isEditing', false);
+			}.bind(this));
+		}
+	},
 	setIsOwner: function() {
 		var _this = this;
 		return this.get('model').get('author').then(function(author) {
